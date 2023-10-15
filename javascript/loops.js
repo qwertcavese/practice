ed2e99cdd91958ccaf788e2095e770d44dc7f79e
qwertcavese <cavese7102@gmail.com>
2023-10-15 11:21:35 +0530

    js

---
+++ javascript/loops.js
@@ -0,0 +1,73 @@
+var i;
+// for(i=0;i<=10;i++)
+// {
+//     console.log(+i);
+//     console.log<"br">
+// }
+// for(i=10;i>=0;i--)
+// {
+//     console.log(+i);
+// }
+for(i=1;i<=5;i++)
+{
+    if(i==1 || i==5){
+        console.log("$");
+    }
+    else{
+        console.log("*");
+    }
+}
+// for(i=1;i<=5;i++)
+// {
+//     if(i==1 || i==5){
+//         document.write("*");
+//     }
+//     else{
+//         document.write("$");
+//     }
+// }
+// for(i=1;i<=5;i++)
+// {
+//     if(i%2==0){
+//         document.write("*");
+//     }
+//     else{
+//         document.write("$");
+//     }
+// }
+// for(i=1;i<=5;i++)
+// {
+//     if(i%3==0)
+//     {
+//         document.write("$");
+//     }
+//     else{
+//         document.write("*");
+//     }
+// }
+// for(i=1;i<=5;i++)
+// {
+//     if(i%2==0){
+//         document.write("0");
+//     }
+//     else{
+//         document.write("1");
+//     }
+// }
+// var a;
+//     for(a=1;a<=5;a++)
+//     {
+//         // document.write(a);
+//         document.write("<br>");
+//         for( var j=1;j<=a;j++)
+//         {
+//             document.write(j);
+//         }
+//     }
+// var i
+// for(i=5;i>=1;i--){
+//     for(j=5;j>=1;j--){
+//         document.write(j);
+//     }
+//     document.write("<br>");
+//     }
